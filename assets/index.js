145be89b7fc837d783f0cce72bd4e39afef73809
xavcz <xavier.cazalot@gmail.com>
2017-04-27 10:45:01 +0200

    create an assets service used for tests

---
+++ assets/index.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const http = require('http');
+const fs = require('fs');
+
+const types = {
+  gif: 'image/gif',
+  jpg: 'image/jpeg',
+  png: 'image/png',
+  svg: 'image/svg+xml',
+};
+
+// exports a simple asset server
+module.exports = http.createServer((req, res) => {
+  // get the image absolute local path & its type
+  const file = path.join(__dirname, req.url);
+  const fileType = types[path.extname(file).slice(1)];
+
+  const stream = fs.createReadStream(file);
+
+  // stream it...
+  stream.on('open', function() {
+    res.setHeader('Content-Type', fileType);
+    stream.pipe(res);
+  });
+
+  // ...or fail!
+  stream.on('error', function() {
+    res.setHeader('Content-Type', 'text/plain');
+    res.statusCode = 404;
+    res.end('Not found');
+  });
+});
